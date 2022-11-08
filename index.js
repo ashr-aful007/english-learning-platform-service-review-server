c3d652aeafa43588afa4557021ee9e5eaf4069c3
ashr-aful007 <ashraful99980@gmail.com>
2022-11-08 20:24:41 +0600

    add all service and limite service route data set up

--- index.js
+++ index.js
@@ -8,10 +8,40 @@ const port = process.env.PORT || 5000
 app.use(cors())
 app.use(express.json())
 
+
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster1.bsfuvd2.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run (){
+    try{
+        const serviceCollection = client.db('english-platfrom').collection('services')
+        app.get('/services', async(req, res) =>{
+            const query = {}
+            const cursor = serviceCollection.find(query)
+            const allservices = await cursor.toArray()
+            res.send(allservices)
+        })
+        app.get('/fewservices', async(req, res) =>{
+            const query ={}
+            const cursor = serviceCollection.find(query)
+            const fewservice = await cursor.limit(3).toArray()
+            res.send(fewservice)
+        })
+    }
+    finally{
+
+    }
+}
+
+run().catch(err => console.log(err))
+
+
 app.get('/', (req, res) =>{
     res.send('server is runnig')
 })
 
-app.listen(port,()=>{
+app.listen(port,()=>{   
      console.log(`server runnig on ${port}`)
 })
