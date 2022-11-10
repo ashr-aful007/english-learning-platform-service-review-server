357e9c45a5295b2c80bcb35f48d54feb8982b917
ashr-aful007 <ashraful99980@gmail.com>
2022-11-10 10:37:45 +0600

    add reviews post

--- index.js
+++ index.js
@@ -10,25 +10,44 @@ app.use(express.json())
 
 
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster1.bsfuvd2.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
 async function run (){
     try{
         const serviceCollection = client.db('english-platfrom').collection('services')
+        const reviewsCollection = client.db('english-platfrom').collection('reviews')
+        //all services
         app.get('/services', async(req, res) =>{
             const query = {}
             const cursor = serviceCollection.find(query)
             const allservices = await cursor.toArray()
             res.send(allservices)
         })
+        //for 3 services
         app.get('/fewservices', async(req, res) =>{
             const query ={}
             const cursor = serviceCollection.find(query)
             const fewservice = await cursor.limit(3).toArray()
             res.send(fewservice)
         })
+        //services serch for id
+        app.get('/services/:id', async(req, res) =>{
+            const id = req.params.id
+            const query = {_id: ObjectId(id)}
+            const service = await serviceCollection.findOne(query)
+            res.send(service)
+        })
+        //store user reviews
+        app.post('/reviews', async(req, res) =>{
+            const service = req.body;
+            const result = await reviewsCollection.insertOne(service)
+            console.log(result)
+            res.send(result)
+        })
+        //get reviwes 
+        
     }
     finally{
 
